87bdee69c5c0c268c226b48623f756f88b5d4130
Gustav Sandström <gussan@pop-os.localdomain>
2022-05-09 07:59:39 +0200

    day2. should refactor to guava input streams

---
+++ src/main/java/com/adventofcode/year2015/year2015/Day2.java
@@ -0,0 +1,75 @@
+package com.adventofcode.year2015.year2015;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day2 {
+
+    record Gift(int height, int width, int length){
+        int wrapperSize(){
+            int side1 = length * width;
+            int side2 = width * height;
+            int side3 = height * length;
+            return 2 * side1 + 2 * side2 + 2 * side3 + smallest(List.of(side1, side2, side3));
+        }
+
+        int ribbonSize(){
+            List<Integer> sides = List.of(length, width, height);
+            int sideSum = sides.stream().reduce(1, (integer, integer2) -> integer*integer2);
+            int smallest = 2 * smallest(sides);
+            int secondSmallest = 2 * smallest(sides, 1);
+
+            return smallest + secondSmallest + sideSum;
+        }
+
+        int smallest(List<Integer> candidates, int smallestIndex){
+            List<Integer> tmp = new ArrayList<>(candidates);
+           tmp.sort(Integer::compareTo);
+           return tmp.get(smallestIndex);
+        }
+
+        int smallest(List<Integer> candidates){
+            return smallest(candidates, 0);
+        }
+    }
+
+    public static int part1(List<String> instructions) {
+        int sum = 0;
+        for (String i: instructions){
+            List<Integer> parts = Arrays.stream(i.split("x")).map(Integer::parseInt).toList();
+            Gift p = new Gift(parts.get(0), parts.get(1), parts.get(2));
+            sum += p.wrapperSize();
+        }
+        return sum;
+    }
+
+    public static int part2(List<String> instructions) {
+        int sum = 0;
+        for (String i: instructions){
+            List<Integer> parts = Arrays.stream(i.split("x")).map(Integer::parseInt).toList();
+            Gift p = new Gift(parts.get(0), parts.get(1), parts.get(2));
+            sum += p.ribbonSize();
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args){
+        try {
+            InputStream i = Day2.class.getClassLoader().getResourceAsStream("day2.txt");
+            Scanner s = new Scanner(i).useDelimiter(System.lineSeparator());
+            List<String> instructions = new ArrayList<>();
+            while (s.hasNextLine()){
+                instructions.add(s.next());
+            }
+            System.out.println("Part1: " + part1(instructions));
+            System.out.println("Part2: " + part2(instructions));
+        } catch (Exception e){
+            System.err.println("Something went poorly");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/adventofcode/year2015/Day2Test.java
@@ -0,0 +1,40 @@
+
+package com.adventofcode.year2015;
+
+import com.adventofcode.year2015.year2015.Day2;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day2Test {
+    @Test
+    public void testExample(){
+        assertEquals("simple case", 58, Day2.part1(List.of("2x3x4")));
+    }
+
+    @Test
+    public void testExample2(){
+        assertEquals("simple case2 ", 43, Day2.part1(List.of("1x1x10")));
+    }
+
+    @Test
+    public void testExampleTwoRows(){
+        assertEquals("simple add ", 43 + 58, Day2.part1(List.of("1x1x10", "2x3x4")));
+    }
+
+    @Test
+    public void testPart2Example(){
+        assertEquals("simple case", 34, Day2.part2(List.of("2x3x4")));
+    }
+
+    @Test
+    public void testPart2Example2(){
+        assertEquals("simple case2 ", 14, Day2.part2(List.of("1x1x10")));
+    }
+    @Test
+    public void testExample2TwoRows(){
+        assertEquals("simple add ", 14 + 34, Day2.part2(List.of("1x1x10", "2x3x4")));
+    }
+}
