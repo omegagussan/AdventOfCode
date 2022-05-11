ef91e078a853c7a7353745d13dcf4ae031663554
Gustav Sandström <gustav.sandstrom@nentgroup.com>
2022-05-11 09:41:45 +0200

    day 6 part 2

--- src/main/java/com/adventofcode/year2015/Day6.java
+++ src/main/java/com/adventofcode/year2015/Day6.java
@@ -10,7 +10,7 @@ public class Day6 {
         OFF,
         ON;
 
-        BulbOperationState fromString(String s){
+        static BulbOperationState fromString(String s){
             var t = s.replaceAll("turn ", "").trim().toUpperCase();
             return BulbOperationState.valueOf(t);
         }
@@ -39,11 +39,60 @@ public class Day6 {
         }
     }
 
+    record Grid2(Map<Pair, Integer> state){
+        void on(int xc, int yx){
+            var p = new Pair(xc, yx);
+            var curr = this.state.getOrDefault(p, 0); //assume missing are off
+            this.state.put(p, curr + 1);
+        }
+
+        void off(int xc, int yx){
+            var p = new Pair(xc, yx);
+            var curr = this.state.getOrDefault(p, 0); //assume missing are off
+            this.state.put(p, Math.max(curr - 1, 0));
+        }
+
+        void toggle(int xc, int yx){
+            var p = new Pair(xc, yx);
+            var curr = this.state.getOrDefault(p, 0); //assume missing are off
+            this.state.put(p, curr + 2);
+        }
+
+        int count(){
+            return this.state.values().stream().reduce(Integer::sum).orElse(0);
+        }
+    }
+
+
     public static int part1(List<String> instructions) {
         var grid = new Grid(new HashMap<>());
         for (String i : instructions){
             String operationString = i.split("\\d+", 2)[0];
-            BulbOperationState operation = BulbOperationState.OFF.fromString(operationString);
+            BulbOperationState operation = BulbOperationState.fromString(operationString);
+            String numberString = i.replaceAll("[^, 0-9]", "").replaceAll("\\s{2,}", " ").trim();
+            int xFrom = Integer.parseInt(numberString.split(" ")[0].split(",")[0]);
+            int xTo = Integer.parseInt(numberString.split(" ")[1].split(",")[0]);
+            int yFrom = Integer.parseInt(numberString.split(" ")[0].split(",")[1]);
+            int yTo = Integer.parseInt(numberString.split(" ")[1].split(",")[1]);
+            for (int x=xFrom; x <= xTo; x++){
+                for (int y=yFrom; y <= yTo; y++){
+                    switch (operation) {
+                        case TOGGLE -> grid.toggle(x, y);
+                        case OFF -> grid.off(x, y);
+                        case ON -> grid.on(x, y);
+                        default -> throw new IllegalArgumentException("This is not good!");
+                    }
+                }
+            }
+        }
+        return grid.count();
+    }
+
+    public static int part2(List<String> instructions) {
+        var grid = new Grid2(new HashMap<>());
+        for (String i : instructions){
+            String operationString = i.split("\\d+", 2)[0];
+            BulbOperationState operation = BulbOperationState.fromString(operationString);
             String numberString = i.replaceAll("[^, 0-9]", "").replaceAll("\\s{2,}", " ").trim();
             int xFrom = Integer.parseInt(numberString.split(" ")[0].split(",")[0]);
             int xTo = Integer.parseInt(numberString.split(" ")[1].split(",")[0]);
@@ -72,6 +121,7 @@ public class Day6 {
                 instructions.add(s.next());
             }
             System.out.println("Part1: " + part1(instructions));
+            System.out.println("Part2: " + part2(instructions));
         } catch (Exception e){
             System.err.println("Something went poorly");
             e.printStackTrace();
