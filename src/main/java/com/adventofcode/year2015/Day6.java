942a104e20fe55f6c16468c5e0bcc9186d181262
Gustav Sandström <gustav.sandstrom@nentgroup.com>
2022-05-11 13:13:40 +0200

    refactor

--- src/main/java/com/adventofcode/year2015/Day6.java
+++ src/main/java/com/adventofcode/year2015/Day6.java
@@ -15,20 +15,44 @@ public class Day6 {
             return BulbOperationState.valueOf(t);
         }
     }
+    interface Operations {
+        void on(int xc, int yx);
+        void off(int xc, int yx);
+        void toggle(int xc, int yx);
+    }
 
     record Pair(int x, int y){}
-    record Grid(Map<Pair, Integer> state){
-        void on(int xc, int yx){
+    static class GridContainer{
+        private final Grid g1;
+        private final Grid2 g2;
+
+
+        GridContainer(Grid g1) {
+            this.g1 = g1;
+            this.g2 = null;
+        }
+
+        GridContainer(Grid2 g2) {
+            this.g1 = null;
+            this.g2 = g2;
+        }
+
+        public Object getGrid(){
+            return this.g1 == null ? this.g2 : this.g1;
+        }
+    }
+    record Grid(Map<Pair, Integer> state) implements Operations{
+        public void on(int xc, int yx){
             var p = new Pair(xc, yx);
             this.state.put(p, 1);
         }
 
-        void off(int xc, int yx){
+        public void off(int xc, int yx){
             var p = new Pair(xc, yx);
             this.state.put(p, 0);
         }
 
-        void toggle(int xc, int yx){
+        public void toggle(int xc, int yx){
             var p = new Pair(xc, yx);
             var curr = this.state.getOrDefault(p, 0); //assume missing are off
             this.state.put(p, curr == 0 ? 1 : 0);
@@ -39,20 +63,20 @@ public class Day6 {
         }
     }
 
-    record Grid2(Map<Pair, Integer> state){
-        void on(int xc, int yx){
+    record Grid2(Map<Pair, Integer> state) implements Operations{
+        public void on(int xc, int yx){
             var p = new Pair(xc, yx);
             var curr = this.state.getOrDefault(p, 0); //assume missing are off
             this.state.put(p, curr + 1);
         }
 
-        void off(int xc, int yx){
+        public void off(int xc, int yx){
             var p = new Pair(xc, yx);
             var curr = this.state.getOrDefault(p, 0); //assume missing are off
             this.state.put(p, Math.max(curr - 1, 0));
         }
 
-        void toggle(int xc, int yx){
+        public void toggle(int xc, int yx){
             var p = new Pair(xc, yx);
             var curr = this.state.getOrDefault(p, 0); //assume missing are off
             this.state.put(p, curr + 2);
@@ -66,30 +90,19 @@ public class Day6 {
 
     public static int part1(List<String> instructions) {
         var grid = new Grid(new HashMap<>());
-        for (String i : instructions){
-            String operationString = i.split("\\d+", 2)[0];
-            BulbOperationState operation = BulbOperationState.fromString(operationString);
-            String numberString = i.replaceAll("[^, 0-9]", "").replaceAll("\\s{2,}", " ").trim();
-            int xFrom = Integer.parseInt(numberString.split(" ")[0].split(",")[0]);
-            int xTo = Integer.parseInt(numberString.split(" ")[1].split(",")[0]);
-            int yFrom = Integer.parseInt(numberString.split(" ")[0].split(",")[1]);
-            int yTo = Integer.parseInt(numberString.split(" ")[1].split(",")[1]);
-            for (int x=xFrom; x <= xTo; x++){
-                for (int y=yFrom; y <= yTo; y++){
-                    switch (operation) {
-                        case TOGGLE -> grid.toggle(x, y);
-                        case OFF -> grid.off(x, y);
-                        case ON -> grid.on(x, y);
-                        default -> throw new IllegalArgumentException("This is not good!");
-                    }
-                }
-            }
-        }
+        followInstructions(instructions, new GridContainer(grid));
         return grid.count();
     }
 
     public static int part2(List<String> instructions) {
         var grid = new Grid2(new HashMap<>());
+        followInstructions(instructions, new GridContainer(grid));
+        return grid.count();
+    }
+
+    private static void followInstructions(List<String> instructions, GridContainer gridContainer) {
+        var grid = gridContainer.getGrid();
+
         for (String i : instructions){
             String operationString = i.split("\\d+", 2)[0];
             BulbOperationState operation = BulbOperationState.fromString(operationString);
@@ -101,15 +114,14 @@ public class Day6 {
             for (int x=xFrom; x <= xTo; x++){
                 for (int y=yFrom; y <= yTo; y++){
                     switch (operation) {
-                        case TOGGLE -> grid.toggle(x, y);
-                        case OFF -> grid.off(x, y);
-                        case ON -> grid.on(x, y);
+                        case TOGGLE -> ((Operations) grid).toggle(x, y);
+                        case OFF -> ((Operations) grid).off(x, y);
+                        case ON -> ((Operations) grid).on(x, y);
                         default -> throw new IllegalArgumentException("This is not good!");
                     }
                 }
             }
         }
-        return grid.count();
     }
 
     public static void main(String[] args){
