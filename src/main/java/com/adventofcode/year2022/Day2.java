c97f891a7b8cfc4c4d784a3781d175e921a39f4b
Gustav Sandström <gustavs@spotify.com>
2022-12-02 07:30:15 +0100

    cleanup

--- src/main/java/com/adventofcode/year2022/Day2.java
+++ src/main/java/com/adventofcode/year2022/Day2.java
@@ -2,117 +2,42 @@ package com.adventofcode.year2022;
 
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.function.Function;
 
 public class Day2 {
     public static final String ROW_DELIMITER = "\n";
 
-    private record Round(String their, String yours){
-        int getDiff(){
-            return  this.yours.chars().findFirst().getAsInt() - (this.their.chars().findFirst().getAsInt() + 23);
-        }
-    }
-
-    private static Function<Round, Integer> yourSignScore() {
-        return round -> switch (round.yours) {
-            case "X" -> 1;
-            case "Y" -> 2;
-            case "Z" -> 3;
-            case default -> throw new RuntimeException("Not a valid sign");
-        };
-    }
-
-    private static Function<String, Round> asRound() {
-        return s -> {
-            var a = s.split(" ");
-            return new Round(a[0], a[a.length - 1]);
-        };
-    }
-
-    private static Function<Round, Integer> getRoundScore() {
-        return round -> {
-            switch (round.getDiff()) {
-                case 0 -> {
-                    return 3;
-                }
-                case 1, -2 -> {
-                    return 6;
-                }
-                case 2, -1 -> {
-                    return 0;
-                }
-                case default -> throw new RuntimeException("Not a valid diff from pair");
-            }
-        };
-    }
-    private static Function<Round, Round> getSignFromOpponent() {
-        return round -> {
-            switch (round.yours) {
-                case "X" -> {
-                    return new Round(round.their, howToLoose(round.their));
-                }
-                case "Y" -> {
-                    return new Round(round.their, howToDraw(round.their));
-                }
-                case "Z" -> {
-                    return new Round(round.their, howToWin(round.their));
-                }
-                case default -> throw new RuntimeException("Not a valid opponents sign");
-            }
-        };
-    }
-
-    static public String howToWin(String argumentSign){
-        return switch (argumentSign){
-            case "A" -> "Y";
-            case "B" -> "Z";
-            case "C" -> "X";
-            default -> throw new IllegalStateException("Unexpected value: " + argumentSign);
-        };
-    }
-
-    static public String howToLoose(String argumentSign){
-        return switch (argumentSign){
-            case "A" -> "Z";
-            case "B" -> "X";
-            case "C" -> "Y";
-            default -> throw new IllegalStateException("Unexpected value: " + argumentSign);
-        };
-    }
-
-    static public String howToDraw(String argumentSign){
-        return switch (argumentSign){
-            case "A" -> "X";
-            case "B" -> "Y";
-            case "C" -> "Z";
-            default -> throw new IllegalStateException("Unexpected value: " + argumentSign);
-        };
-    }
-
-    private static Function<Round, Integer> usingElfKnowledge() {
-        return round -> switch (round.yours) {
-            case "X" -> 0;
-            case "Y" -> 3;
-            case "Z" -> 6;
-            case default -> throw new RuntimeException("Not a valid sign from you");
-        };
-    }
-
     public static int part1(String instructions) {
-        var rounds = Arrays.stream(instructions.split(ROW_DELIMITER)).toList();
-        var signScore = rounds.stream().map(asRound()).map(yourSignScore()).mapToInt(Integer::valueOf).sum();
+        return Arrays.stream(instructions.split(ROW_DELIMITER)).toList().stream().map(row -> switch (row){
+        case "A X" -> 3 + 1;
+        case "A Y" -> 6 + 2;
+        case "A Z" -> 0 + 3;
 
-        var winScore = rounds.stream().map(asRound()).map(getRoundScore()).mapToInt(Integer::valueOf).sum();
-        return winScore + signScore;
+        case "B X" -> 0 + 1;
+        case "B Y" -> 3 + 2;
+        case "B Z" -> 6 + 3;
+
+        case "C X" -> 6 + 1;
+        case "C Y" -> 0 + 2;
+        case "C Z" -> 3 + 3;
+        default -> throw new IllegalStateException("This cant happen!");
+    }).mapToInt(Integer::valueOf).sum();
     }
 
     public static int part2(String instructions) {
-        var rounds = Arrays.stream(instructions.split(ROW_DELIMITER)).toList();
-        var winScore = rounds.stream().map(asRound()).map(usingElfKnowledge()).mapToInt(Integer::valueOf).sum();
+        return Arrays.stream(instructions.split(ROW_DELIMITER)).toList().stream().map(row -> switch (row){
+            case "A X" -> 0 + 3;
+            case "A Y" -> 3 + 1;
+            case "A Z" -> 6 + 2;
 
-        var signState = rounds.stream().map(asRound()).map(getSignFromOpponent()).toList();
-        var signScore = signState.stream().map(yourSignScore()).mapToInt(Integer::valueOf).sum();
-        return winScore + signScore;
+            case "B X" -> 0 + 1;
+            case "B Y" -> 3 + 2;
+            case "B Z" -> 6 + 3;
+
+            case "C X" -> 0 + 2;
+            case "C Y" -> 3 + 3;
+            case "C Z" -> 6 + 1;
+            default -> throw new IllegalStateException("This cant happen!");
+        }).mapToInt(Integer::valueOf).sum();
     }
 
     public static void main(String[] args){
