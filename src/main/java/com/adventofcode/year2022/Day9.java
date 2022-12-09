84dffe8371651b25581eb2ed15746f3a1d46945e
Gustav Sandström <gustavs@spotify.com>
2022-12-09 07:10:26 +0100

    wip

--- src/main/java/com/adventofcode/year2022/Day9.java
+++ src/main/java/com/adventofcode/year2022/Day9.java
@@ -1,9 +1,48 @@
 package com.adventofcode.year2022;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Day9 {
+    public record Coord(Integer i, Integer j){
+        <T> T getValue(T[][] matrix){
+            return matrix[i][j];
+        }
 
+        Coord move(Integer i, Integer j){
+            return new Coord(this.i + i, this.j + j);
+        }
+
+        Coord move(Coord delta){
+            if (delta.i * delta.i > delta.j * delta.j){
+                int unitI = (int) (delta.i / Math.sqrt(delta.i * delta.i));
+                return new Coord(this.i + unitI, this.j);
+            }
+            int unitJ = (int) (delta.j / Math.sqrt(delta.j * delta.j));
+            return new Coord(this.i, this.j + unitJ);
+        }
+
+        Coord moveDiagonal(Coord delta){
+            int unitJ = (int) (delta.j / Math.sqrt(delta.j * delta.j));
+            int unitI = (int) (delta.i / Math.sqrt(delta.i * delta.i));
+            return new Coord(this.i + unitI, this.j + unitJ);
+        }
+
+        boolean isTouching(){
+            return Math.abs(i) == 1 || Math.abs(j) == 1;
+        }
+
+        int magnitude(){
+            return i*i + j*j;
+        }
+
+        static Coord compare(Coord a, Coord b){
+            return new Coord(a.i - b.i, a.j - b.j);
+        }
+
+
+    }
     public static final String ROW_DELIMITER = "\n";
 
     public static int part2(String instructions) {
@@ -12,7 +51,37 @@ public class Day9 {
 
 
     public static int part1(String instructions) {
-        return 2;
+        final Coord[] head = {new Coord(0, 0)};
+        final Coord[] tail = {new Coord(0, 0)};
+        var taiVisited = new HashSet<Coord>();
+
+        Arrays.stream(instructions.split(ROW_DELIMITER)).forEach(a -> {
+            int value = Integer.parseInt(a.replaceAll("[^0-9]", ""));
+            for (int i = 0; i < value; i++){
+                followHead(head, tail, taiVisited);
+                switch (a.substring(0, 1)) {
+                    case "U" -> head[0] = head[0].move(1, 0);
+                    case "D" -> head[0] = head[0].move(-1, 0);
+                    case "R" -> head[0] = head[0].move(0, 1);
+                    case "L" -> head[0] = head[0].move(0, -1);
+                };
+                followHead(head, tail, taiVisited);
+            }
+        });
+        followHead(head, tail, taiVisited);
+
+        return taiVisited.size();
+    }
+
+    private static void followHead(Coord[] head, Coord[] tail, HashSet<Coord> taiVisited) {
+        var delta = Coord.compare(head[0], tail[0]);
+        if (delta.magnitude() > 4){ //diagonal
+            tail[0] = tail[0].moveDiagonal(delta);
+            taiVisited.add(tail[0]);
+        } else if (!delta.isTouching()) {
+            tail[0] = tail[0].move(delta);
+            taiVisited.add(tail[0]);
+        }
     }
 
     public static void main(String[] args){
--- src/test/java/com/adventofcode/year2022/Day9Test.java
+++ src/test/java/com/adventofcode/year2022/Day9Test.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 public class Day9Test {
     @Test
     public void testExample(){
-    assertEquals("simple case", 15, Day9.part1("A Y\n" + "B X\n" + "C Z"));
+    assertEquals(
+        "simple case",
+        13,
+        Day9.part1("R 4\n" + "U 4\n" + "L 3\n" + "D 1\n" + "R 4\n" + "D 1\n" + "L 5\n" + "R 2\n"));
     }
 }
 
