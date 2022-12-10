4213822750f556b086a4686a354e5f0bfdc00d5e
Gustav Sandström <gustavs@spotify.com>
2022-12-10 07:13:24 +0100

    wip2

--- src/main/java/com/adventofcode/year2022/Day10.java
+++ src/main/java/com/adventofcode/year2022/Day10.java
@@ -1,14 +1,14 @@
 
 package com.adventofcode.year2022;
 
+import com.google.common.collect.Lists;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Day10 {
     public static final String ROW_DELIMITER = "\n";
@@ -40,8 +40,32 @@ public class Day10 {
         return answer.stream().mapToInt(Integer::intValue).sum();
     }
 
-    public static int part2(String instructions) {
-        return 2;
+    public static String part2(String instructions) {
+        var CRT = new ArrayList<String>();
+        var memory = new ArrayList<Integer>();
+        var registry = new AtomicReference<>(1);
+
+        Arrays.stream(instructions.split(ROW_DELIMITER)).forEach(r -> {
+            if (Math.abs(registry.get() % 40 - CRT.size() % 40) < 2){
+                CRT.add("#");
+            } else {
+                CRT.add(".");
+            }
+            if (r.startsWith("noop")){
+                //do nothing
+            } else {
+                int value = Integer.parseInt(r.replaceAll("[^0-9-]", ""));
+                memory.add(registry.get());
+                if (Math.abs(registry.get() % 40 - CRT.size() % 40) < 2){
+                    CRT.add("#");
+                } else {
+                    CRT.add(".");
+                }
+                registry.updateAndGet(v -> v + value);
+            }
+            memory.add(registry.get());
+        });
+    return String.join("", Lists.partition(CRT, 40).stream().map(strings -> String.join("", strings) + "\n").toList());
     }
 
     public static void main(String[] args){
@@ -49,7 +73,8 @@ public class Day10 {
             InputStream i = Day10.class.getClassLoader().getResourceAsStream("2022/day10.txt");
             String instructions = new String(i.readAllBytes());
             System.out.println("Part1: " + part1(instructions));
-            System.out.println("Part2: " + part2(instructions));
+            System.out.println("Part2: ");
+            System.out.println(part2(instructions));
         } catch (Exception e){
             e.printStackTrace();
         }
