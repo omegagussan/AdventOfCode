0266dc591f3375a0be8b9eb19f141ebb38662b7d
Gustav Sandström <gustavs@spotify.com>
2022-12-13 21:31:00 +0100

    wip

--- src/main/java/com/adventofcode/year2022/Day13.java
+++ src/main/java/com/adventofcode/year2022/Day13.java
@@ -3,21 +3,14 @@ package com.adventofcode.year2022;
 import com.google.common.collect.Streams;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import javax.swing.JComponent;
 import org.javatuples.Pair;
 
 public class Day13 {
 
     public static final String ROW_DELIMITER = "\n";
-    public static final String ELEM_SEPARATOR = ",";
 
     private static boolean isNumber(String s) {
         s = s.trim();
@@ -38,8 +31,7 @@ public class Day13 {
             .forEach(p -> {
                 var left = p.getValue1().split(ROW_DELIMITER)[0];
                 var right = p.getValue1().split(ROW_DELIMITER)[1];
-                var corr = isCorrect(left, right, true);
-                if (corr){
+                if (isCorrect(left, right, true)){
                     noIntervention.add(p.getValue0());
                 }
             });
@@ -51,41 +43,38 @@ public class Day13 {
         return isCorrect(left, right, false);
     }
 
-    static boolean isCorrect(String left, String right, boolean isOuter) {
-        if (left.isEmpty() || "[]".equals(left)){
-            return true;
-        } else if(right.isEmpty() || "[]".equals(right)){
-            return false;
-        } else if (isNumber(left) && isNumber(right)){
-            int leftInt = Integer.parseInt(left.replaceAll("[^0-9]", ""));
-            int rightInt = Integer.parseInt(right.replaceAll("[^0-9]", ""));
-            return rightInt >= leftInt;
-        } else if (isArray(left) && isArray(right)){
+  static boolean isCorrect(String left, String right, boolean isOuter) {
+    if ("[]".equals(left)) {
+      return true;
+    } else if ("[]".equals(right)) {
+      return false;
+    } else if (isNumber(left) && isNumber(right)) {
+      int leftInt = Integer.parseInt(left.replaceAll("[^0-9]", ""));
+      int rightInt = Integer.parseInt(right.replaceAll("[^0-9]", ""));
+      return rightInt >= leftInt;
+    } else if (isArray(left) && isNumber(right)) {
+      return isCorrect(left, String.format("[%s]", right));
+    } else if (isNumber(left) && isArray(right)) {
+      return isCorrect(String.format("[%s]", left), right);
+    } else if (isArray(left) && isArray(right)) {
+      var leftL = parseStringArrays(left.substring(1, left.length() - 1));
+      var rightL = parseStringArrays(right.substring(1, right.length() - 1));
 
-            parseStringArrays(left.substring(1, left.length() - 1));
+      List<Boolean> x =
+          Streams.zip(leftL.stream(), rightL.stream(), Pair::new)
+              .map(comp -> isCorrect(comp.getValue0(), comp.getValue1()))
+              .toList();
 
-            var leftL = parseStringArrays(left.substring(1, left.length() - 1));
-
-            var rightL =  parseStringArrays(right.substring(1, right.length() - 1));
-
-            List<Boolean> x = Streams.zip(leftL.stream(), rightL.stream(), Pair::new)
-                .map(comp -> isCorrect(comp.getValue0(), comp.getValue1())).toList();
-
-            if(isOuter){
-                return x.stream()
-                    .filter(b -> !b).findFirst()
-                    .orElse(leftL.size() <= rightL.size());
-            }
-            return x.stream()
-                .filter(b -> !b).findFirst()
-                .orElse(true); //leftL.size() <= rightL.size());
-        } else if (isArray(left) && isNumber(right)){
-            return isCorrect(left, String.format("[%s]", right));
-        } else if (isNumber(left) && isArray(right)){
-            return isCorrect(String.format("[%s]", left), right);
-        }
-        throw new RuntimeException("what dis? " + left + " | " +  right);
+      if (x.stream().anyMatch(b -> !b)) {
+        return false;
+      }
+      if (isOuter) {
+        return rightL.size() >= leftL.size();
+      }
+      return true;
     }
+    throw new RuntimeException("what dis? " + left + " | " + right);
+}
 
     static List<String> parseStringArrays(String x) {
         StringBuilder stringBuilder = new StringBuilder();
