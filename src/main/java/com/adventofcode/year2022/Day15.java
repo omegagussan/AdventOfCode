45c1c41ec37cdc7e63cc22f68c54813ceb867131
Gustav Sandström <gustavs@spotify.com>
2022-12-16 16:44:20 +0100

    day15 part2

--- src/main/java/com/adventofcode/year2022/Day15.java
+++ src/main/java/com/adventofcode/year2022/Day15.java
@@ -3,8 +3,12 @@ package com.adventofcode.year2022;
 import com.adventofcode.utils.Point;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -17,8 +21,63 @@ public class Day15 {
 
   public static final String ROW_DELIMITER = "\n";
 
-  public static int part2(String instructions) {
-    return 2;
+  static Stream<Point> getVicinity(Point p, int d) {
+    return IntStream.rangeClosed(0, d)
+        .mapToObj(
+            x ->
+                List.of(
+                    new Point(p.i() - x, p.j() - (d - x)),
+                    new Point(p.i() - x, p.j() + (d - x)),
+                    new Point(p.i() + x, p.j() - (d - x)),
+                    new Point(p.i() + x, p.j() + (d - x))))
+        .flatMap(Collection::stream)
+        .collect(Collectors.toSet())
+        .stream();
+  }
+
+  public static long part2(String instructions, int maxValue) {
+    var sensorBeaconPairs = parseInput(instructions).toList();
+    var beacons =
+        sensorBeaconPairs.stream()
+            .map(l -> new Point(l.get(2), l.get(3)))
+            .collect(Collectors.toSet());
+    var sensors =
+        sensorBeaconPairs.stream()
+            .map(l -> new Point(l.get(0), l.get(1)))
+            .collect(Collectors.toSet());
+
+    var sensorDistancePairs =
+        sensorBeaconPairs.stream()
+            .map(
+                row -> {
+                  Integer manhattanDistance =
+                      Math.addExact(
+                          Math.abs(row.get(0) - row.get(2)), Math.abs(row.get(1) - row.get(3)));
+                  return new Pair<>(new Point(row.get(0), row.get(1)), manhattanDistance);
+                })
+            .toList();
+
+    AtomicReference<Optional<Long>> freq = new AtomicReference<>(Optional.empty());
+    sensorDistancePairs.forEach(
+        sdp -> {
+          if (freq.get().isPresent() && freq.get().get() > 0) {
+            return;
+          }
+
+          Optional<Long> newFreq =
+              getVicinity(sdp.getValue0(), sdp.getValue1() + 1).toList().stream()
+                  .filter(p1 -> 0 <= p1.i() && p1.i() <= maxValue)
+                  .filter(p1 -> 0 <= p1.j() && p1.j() <= maxValue)
+                  .filter(p1 -> isRangeEligability(sensorDistancePairs, p1))
+                  .toList()
+                  .stream()
+                  .map(p -> Math.addExact(p.j(), Math.multiplyExact(4_000_000L, p.i())))
+                  .findAny();
+          if (newFreq.isPresent()) {
+            freq.set(newFreq);
+          }
+        });
+    return freq.get().get();
   }
 
   public static int part1(String instruction, int lineToSearch) {
@@ -59,16 +118,26 @@ public class Day15 {
         .forEach(
             i -> {
               var pt = new Point(i, lineToSearch);
-              var rangeEligability =
-                  sensorDistancePairs.stream().map(p -> new Pair<>(Math.addExact(
-                          Math.abs(p.getValue0().i() - pt.i()),
-                          Math.abs(p.getValue0().j() - pt.j())), p.getValue1()))
-                      .allMatch(p2 -> p2.getValue0() > p2.getValue1());
-              if (!rangeEligability || beacons.contains(pt) || sensors.contains(pt)) {
+              if (!isRangeEligability(sensorDistancePairs, pt)
+                  || beacons.contains(pt)
+                  || sensors.contains(pt)) {
                 possibleAdditionalBeacons.add(pt);
               }
             });
-    return possibleAdditionalBeacons.size() - (int) beacons.stream().filter(point -> point.j() == lineToSearch).count();
+    return possibleAdditionalBeacons.size()
+        - (int) beacons.stream().filter(point -> point.j() == lineToSearch).count();
+  }
+
+  private static boolean isRangeEligability(
+      List<Pair<Point, Integer>> sensorDistancePairs, Point pt) {
+    return sensorDistancePairs.stream()
+        .map(
+            p ->
+                new Pair<>(
+                    Math.addExact(
+                        Math.abs(p.getValue0().i() - pt.i()), Math.abs(p.getValue0().j() - pt.j())),
+                    p.getValue1()))
+        .allMatch(p2 -> p2.getValue0() > p2.getValue1());
   }
 
   @NotNull
@@ -91,7 +160,7 @@ public class Day15 {
       InputStream i = Day15.class.getClassLoader().getResourceAsStream("2022/day15.txt");
       String instructions = new String(i.readAllBytes());
       System.out.println("Part1: " + part1(instructions, 2000000));
-      System.out.println("Part2: " + part2(instructions));
+      System.out.println("Part2: " + part2(instructions, 4000000));
     } catch (Exception e) {
       e.printStackTrace();
     }
--- src/test/java/com/adventofcode/year2022/Day15Test.java
+++ src/test/java/com/adventofcode/year2022/Day15Test.java
@@ -1,8 +1,10 @@
 package com.adventofcode.year2022;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.adventofcode.utils.Point;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import java.util.List;
 import java.util.Set;
 import org.javatuples.Pair;
@@ -34,7 +36,7 @@ public class Day15Test {
     }
 
     @Test
-    public void testExample(){
+    public void testPart1(){
         var expected = Day15.part1(
             """
                 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -53,6 +55,54 @@ public class Day15Test {
                 Sensor at x=20, y=1: closest beacon is at x=15, y=3
                 """, 10);
         assertEquals(26, expected);
+    }
+
+    @Test
+    public void testPart2(){
+        var expected = Day15.part2(
+            """
+                Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                Sensor at x=9, y=16: closest beacon is at x=10, y=16
+                Sensor at x=13, y=2: closest beacon is at x=15, y=3
+                Sensor at x=12, y=14: closest beacon is at x=10, y=16
+                Sensor at x=10, y=20: closest beacon is at x=10, y=16
+                Sensor at x=14, y=17: closest beacon is at x=10, y=16
+                Sensor at x=8, y=7: closest beacon is at x=2, y=10
+                Sensor at x=2, y=0: closest beacon is at x=2, y=10
+                Sensor at x=0, y=11: closest beacon is at x=2, y=10
+                Sensor at x=20, y=14: closest beacon is at x=25, y=17
+                Sensor at x=17, y=20: closest beacon is at x=21, y=22
+                Sensor at x=16, y=7: closest beacon is at x=15, y=3
+                Sensor at x=14, y=3: closest beacon is at x=15, y=3
+                Sensor at x=20, y=1: closest beacon is at x=15, y=3
+                """, 20);
+        assertEquals(56000011, expected);
+    }
+
+    @Test
+    public void testGetVicinity(){
+        var expected = Day15.getVicinity(new Point(0,0), 1).toList();
+        assertEquals(4, expected.size());
+        assertFalse(expected.contains(new Point(0, 0)));
+    }
+
+    @Test
+    public void testGetVicinity2(){
+        var expected = Day15.getVicinity(new Point(0,0), 2).toList();
+        assertEquals(8, expected.size());
+    }
+
+    @Test
+    public void testGetVicinity3(){
+        var expected = Day15.getVicinity(new Point(0,0), 3).toList();
+        assertEquals(12, expected.size());
+    }
+    @Test
+    public void testGetVicinityOffset(){
+        var expected = Day15.getVicinity(new Point(1,0), 1).toList();
+        assertTrue(expected.contains(new Point(2, 0)));
+        assertTrue(expected.contains(new Point(0, 0)));
+        assertFalse(expected.contains(new Point(-1, 0)));
     }
 }
 
