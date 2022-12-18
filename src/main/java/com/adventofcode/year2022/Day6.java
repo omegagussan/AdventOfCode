05a7fc751a0ecd0aa9a3aa74cf8a79c574ac286f
Gustav Sandström <gustavs@spotify.com>
2022-12-18 20:59:50 +0100

    old cleanup

--- src/main/java/com/adventofcode/year2022/Day6.java
+++ src/main/java/com/adventofcode/year2022/Day6.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.IntStream;
+import org.javatuples.Pair;
 
 public class Day6 {
     public static int part1(String instructions) {
@@ -15,16 +16,10 @@ public class Day6 {
         return solution(instructions, 14);
     }
 
-    record Pair(Integer startOfSet, Set setOfChar){
-        int endOfSet(int messageLength){
-            return this.startOfSet + messageLength;
-        }
-    }
-
     private static int solution(String instructions, int messageLength) {
         return IntStream.range(0, instructions.length()- messageLength + 1)
-            .mapToObj(i -> new Pair(i, setOfSubset(instructions, messageLength, i)))
-            .filter(p -> p.setOfChar.size() == messageLength).findFirst().orElseThrow(IllegalStateException::new).endOfSet(messageLength);
+            .mapToObj(i -> new Pair<>(i, setOfSubset(instructions, messageLength, i)))
+            .filter(p -> p.getValue1().size() == messageLength).map(p -> p.getValue0() + messageLength).findFirst().orElseThrow(IllegalStateException::new);
     }
 
     private static HashSet<String> setOfSubset(String instructions, int messageLength, int i) {
