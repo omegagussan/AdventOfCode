5ba8baf3e37cca162090f641496d2692e37e6720
Gustav Sandström <gustavs@spotify.com>
2022-12-25 09:59:04 +0100

    cleanup

--- src/main/java/com/adventofcode/year2022/Day25.java
+++ src/main/java/com/adventofcode/year2022/Day25.java
@@ -14,51 +14,50 @@ public class Day25 {
 
   public static final String ROW_DELIMITER = "\n";
   public static final long BASE = 5;
+  public static final int HIGHEST_TOKEN_VALUE = 2;
 
   static long fromSNAFU(String string){
-    long res = 0;
-    long mult = 0;
-    Stack<String> aa = Arrays.stream(string.split("")).collect(Collectors.toCollection(Stack::new));
-    while(aa.size() > 0){
-      var elem = aa.pop();
-      long a = (long) Math.pow(BASE, mult);
-      switch (elem) {
-        case "=" -> {
-          res += Math.multiplyExact(-2L , a);
-        }
-        case "-" -> {
-          res += -a;
-        }
-        case "1" -> {
-          res += a;
-        }
-        case "2" -> {
-          res += Math.multiplyExact(2L , a);
-        }
-      }
-      mult += 1;
+    long sum = 0;
+    long baseMultiplication = 0;
+    Stack<String> stack = Arrays.stream(string.split("")).collect(Collectors.toCollection(Stack::new));
+    while(stack.size() > 0){
+      var elem = stack.pop();
+      long multiBase = (long) Math.pow(BASE, baseMultiplication);
+      sum += decode(elem, multiBase);
+      baseMultiplication += 1;
     }
-    return res;
+    return sum;
+  }
+
+  private static long decode(String elem, long factor) {
+    return switch (elem) {
+      case "=" -> Math.multiplyExact(-2L , factor);
+      case "-" -> -factor;
+      case "1" -> factor;
+      case "2" -> Math.multiplyExact(2L , factor);
+      case "0" -> 0;
+      default -> throw new IllegalStateException("Unexpected value: " + elem);
+    };
   }
 
 
   static String toSNAFU(long decimal){
-    var a = new ArrayList<Long>();
+    var state = new ArrayList<Long>();
     while (decimal > 0){
-      a.add(decimal % BASE);
+      state.add(decimal % BASE);
       decimal = Math.floorDiv(decimal, BASE);
     }
-    a.add(0L);
-    Collections.reverse(a);
-    while (a.stream().anyMatch(integer -> integer > 2)){
-      IntStream.range(0, a.size()).forEach(i -> {
-        if (a.get(i) > 2){
-          a.set(i-1, a.get(i-1) + 1);
-          a.set(i, a.get(i) - 5);
+    state.add(0L);
+    Collections.reverse(state);
+    while (state.stream().anyMatch(integer -> integer > HIGHEST_TOKEN_VALUE)){
+      IntStream.range(0, state.size()).forEach(i -> {
+        if (state.get(i) > HIGHEST_TOKEN_VALUE){
+          state.set(i-1, state.get(i-1) + 1);
+          state.set(i, state.get(i) - BASE);
         }
       });
     }
-    String str = a.stream().map(Day25::encode).reduce("", (x, y) -> x + y);
+    String str = state.stream().map(Day25::encode).reduce("", (x, y) -> x + y);
     return !str.equals("0") ? StringUtils.stripStart(str, "0") : "0";
   };
 
@@ -84,16 +83,11 @@ public class Day25 {
         .map(Day25::fromSNAFU).mapToLong(Long::valueOf).sum();
   }
 
-  public static Integer part2(String instructions){
-    return 1;
-  }
-
   public static void main(String[] args) {
     try {
       InputStream i = Day25.class.getClassLoader().getResourceAsStream("2022/day25.txt");
       String instructions = new String(i.readAllBytes());
       System.out.println("Part1: " + part1(instructions));
-      System.out.println("Part2: " + part2(instructions));
     } catch (Exception e) {
       e.printStackTrace();
     }
