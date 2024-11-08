2efd46d12dd9fb9311d646f488a4e7ea8968f3c9
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2024-11-08 17:50:07 +0100

    part 2

--- src/main/java/com/adventofcode/year2017/Day6.java
+++ src/main/java/com/adventofcode/year2017/Day6.java
@@ -2,6 +2,7 @@ package com.adventofcode.year2017;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Day6 {
@@ -10,6 +11,7 @@ public class Day6 {
     public static void main(String... args) {
         Day6 day6 = new Day6();
         System.out.println(part1(day6.input));
+        System.out.println(part2(day6.input));
     }
 
     public static int getMaxIndex(List<Integer> given){
@@ -44,5 +46,28 @@ public class Day6 {
             count++;
         }
         return count;
+    }
+
+    public static int part2(String input) {
+        var seen = new HashMap<String, Integer>();
+        var banks = new ArrayList<>(Arrays.stream(input.split("\t")).map(Integer::parseInt).toList());
+        seen.put(banks.toString(), 0);
+
+        int count = 1;
+        //while(!seen.contains(banks.toString())){
+        while (true){
+            int curr = getMaxIndex(banks);
+            int fwd = banks.get(curr);
+            banks.set(curr, 0);
+            for (int j=1; j <= fwd; j++){
+                int index = (curr + j) % banks.size();
+                banks.set(index, banks.get(index) + 1);
+            }
+            if (seen.containsKey(banks.toString())){
+                return count - seen.get(banks.toString());
+            }
+            seen.put(banks.toString(), count);
+            count++;
+        }
     }
 }
